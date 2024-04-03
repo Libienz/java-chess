6659e1fc269aa987d0326a5faf79e7ab31b089ed
libienz <leekh10195@gmail.com>
2024-04-03 14:46:59 +0900

    feat: 저장과 조회를 Entity를 통해 수행하도록 수정

--- src/main/java/chess/repository/PieceRepository.java
+++ src/main/java/chess/repository/PieceRepository.java
@@ -1,12 +1,8 @@
 package chess.repository;
 
 import chess.domain.board.ChessBoard;
-import chess.domain.piece.Piece;
-import chess.domain.position.Position;
 import chess.repository.entity.PieceEntity;
 import chess.repository.mapper.ChessBoardMapper;
-import chess.repository.mapper.PieceMapper;
-import chess.repository.mapper.PositionMapper;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,19 +23,20 @@ public class PieceRepository {
     public void saveChessBoard(ChessBoard chessBoard) {
         try (Connection connection = connectionManager.getConnection()) {
             chessBoard.getBoard().keySet()
-                    .forEach(position -> savePiece(chessBoard.findPieceByPosition(position), position, connection));
+                    .forEach(position -> savePiece(PieceEntity.of(chessBoard.findPieceByPosition(position), position),
+                            connection));
         } catch (SQLException e) {
             throw new RuntimeException("기물 저장 과정 중 오류 발생");
         }
     }
 
-    private void savePiece(Piece piece, Position position, Connection connection) {
+    private void savePiece(PieceEntity pieceEntity, Connection connection) {
         String query = String.format("INSERT INTO %s (position, team, type) VALUES (?, ?, ?)", TABLE_NAME);
 
         try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-            pstmt.setString(1, PositionMapper.mapPositionToValue(position));
-            pstmt.setString(2, PieceMapper.mapPieceTeamToValue(piece));
-            pstmt.setString(3, PieceMapper.mapPieceTypeToValue(piece));
+            pstmt.setString(1, pieceEntity.getPosition());
+            pstmt.setString(2, pieceEntity.getTeam());
+            pstmt.setString(3, pieceEntity.getType());
             pstmt.execute();
         } catch (SQLException e) {
             throw new RuntimeException("기물 저장 과정 중 오류 발생");
